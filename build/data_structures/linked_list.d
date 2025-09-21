6943096233c08a15ecf77647f8e10c10e3a0800c
Francois THIERREE <frthierr@f1br8s5.clusters.42paris.fr>
2025-09-21 17:14:40 +0200

    chore: add .gitignore and untrack build artifacts

--- build/data_structures/linked_list.d
+++
@@ -1,4 +0,0 @@
-build/data_structures/linked_list.o: lib/data_structures/linked_list.c \
-  lib/data_structures/linked_list.h
-
-lib/data_structures/linked_list.h:
--- build/malloc.d
+++
@@ -1,6 +0,0 @@
-build/malloc.o: lib/malloc.c lib/malloc.h \
-  lib/data_structures/linked_list.h
-
-lib/malloc.h:
-
-lib/data_structures/linked_list.h:
